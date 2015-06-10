4279bdf7c57be949c22f3d20fa3a1b90fa91c611
lijian16 <lj2lin@qq.com>
2015-06-11 00:38:50 +0800

    增加输入框

--- cgi-bin/main.py
+++ cgi-bin/main.py
@@ -6,3 +6,10 @@ print start_response()
 print start_div("center", "margin-top:40px;")
 print img("../views/test.jpg")
 print end_div()
+
+print start_div("center", "margin-top:60px;")
+print input_label("Num1", "adder-1")
+print "+"
+print input_label("Num2", "adder-2")
+print end_div()
+
