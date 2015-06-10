b6ddf7e0d2ffb3e6fe2014e98c52c6018a2d4697
lijian16 <lj2lin@qq.com>
2015-06-11 00:32:17 +0800

    增加模式，图片显示居中

--- cgi-bin/main.py
+++ cgi-bin/main.py
@@ -2,7 +2,7 @@
 import cgi,cgitb
 from res import *
 
-print "Content-type : text/html \n\n"
+print start_response()
+print start_div("center", "margin-top:40px;")
 print img("../views/test.jpg")
-
-print img("../views/test.jpg")
+print end_div()
